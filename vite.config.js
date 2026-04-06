74de805cad79324171d5b886c775ec0ad5ac8410
Maki <maki_22@hotmail.fr>
2026-04-06 02:07:14 +0200

    feat: add fanzine, charte, and morceaux pages and update navigation links

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         playlists: resolve(__dirname, 'playlists.html'),
+        fanzine: resolve(__dirname, 'fanzine.html'),
+        charte: resolve(__dirname, 'charte.html'),
+        morceaux: resolve(__dirname, 'morceaux.html'),
       }
     }
   }
